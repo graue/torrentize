d2c16789a57a3c5a35213acdac5171e49a2124fc
Scott Feeney <graue@oceanbase.org>
2008-09-11 13:59:34 -0500

    When skipping a non-regular file, print its type.
    darcs-hash:20080911185934-bbafa-414fbb331b58cbf905581d19f75ee2a80b464e34

--- filelist.c
+++ filelist.c
@@ -66,10 +66,11 @@ static void add_dir(const char *dirname, const char *prefix)
 		}
 		else
 		{
-			warnx("\rskipping non-regular file %s%s%s",
+			warnx("\rskipping non-regular file %s%s%s (type %d)",
 				prefix,
 				strlen(prefix) == 0 ? "" : "/",
-				de->d_name);
+				de->d_name,
+				de->d_type);
 		}
 	}
 
